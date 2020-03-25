2525435e18305fe586ba1b8c774df949d2a53237
Christian <christiannordfors@gmail.com>
2020-03-24 22:46:12 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+import { registerLocaleData } from '@angular/common';
+import localFr from '@angular/common/locales/fr';
+import localEs from '@angular/common/locales/es';
+
+registerLocaleData(localEs);
+registerLocaleData(localFr);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombre: string= 'Christian Nordfors';
+  nombre2: string= 'chrIsTIan BerNaRDo NORdfOrs';
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI: number = Math.PI;
+  porcentaje: number = 0.235;
+  salario: number = 1234.5;
+  fecha: Date = new Date();
+  activar: boolean = false;
+  idioma: string = 'es';
+  videoUrl: string = 'https://www.youtube.com/embed/3PvNAOJZ04c';
+
+  valorPromesa = new Promise<string>( (resolve) => {
+
+    setTimeout(() =>{
+      resolve('llegó la data de la promesa');
+    }, 4500);
+  });
+
+  persona = {
+    nombre: 'Christian',
+    apellido: 'Nordfors',
+    edad: 31,
+    direccion: {
+      calle: 'C.F.',
+      numero: 123
+    }
+  }
 }
